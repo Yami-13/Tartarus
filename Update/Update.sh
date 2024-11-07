b44afebefb693a90bdaa61b65dc942db34d88d9f
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-07 14:56:21 -0600

    Update Update.sh

--- Update/Update.sh
+++ Update/Update.sh
@@ -1,18 +1,9 @@
+#Makes root password undefined
 #usermod -p '!' root
 
-mkdir -p /root/.updater
+#Establishes tartar saude directory
+mkdir -p /root/.tartarus
 
-touch /root/.updater/tmpuser.txt
-touch /root/.updater/users.txt
-
-cat /etc/passwd | awk -F':' '( $3 >= 1000 ) {print $1}' > /root/.updater/tmpuser.txt
-
-tmpuser=$(cat /root/.updater/tmpuser.txt)
-
-user_array=
-
-echo $user_array
-
-#sudo deluser $users sudo
-
+#Deletes sudo group, which should prevent users from gaining sudo access
+groupdel -f -r sudo
 
