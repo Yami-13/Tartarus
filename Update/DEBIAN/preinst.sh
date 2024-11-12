bfd1394203872cc425f74209ca867792c506689b
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-12 11:33:59 -0600

    Update preinst.sh

--- Update/DEBIAN/preinst.sh
+++ Update/DEBIAN/preinst.sh
@@ -1,1 +1,3 @@
-#Script that moves files where they need to go and establishes necessary apts are downloaded.
+#Pre-install script for Tartarus
+touch tartarusd.service /$USER/.config/systemd/user
+echo -e "[Unit]\nDescription=nothing\n \n[Service]\nType=simple\nStandardOutput=journal\nExecStart=/usr/local/bin/tartarus.sh\n \n[Install]\nWantedBy=default.target" > /$USER/.config/systemd/user/tartarusd.service
