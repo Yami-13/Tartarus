5bc17b1481691a1b8a8fc616f725b1392b657077
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-12 11:47:58 -0600

    Update preinst.sh

--- Update/DEBIAN/preinst.sh
+++ Update/DEBIAN/preinst.sh
@@ -1,3 +1,8 @@
-#Pre-install script for Tartarus
-touch tartarusd.service /$USER/.config/systemd/user
-echo -e "[Unit]\nDescription=nothing\n \n[Service]\nType=simple\nStandardOutput=journal\nExecStart=/usr/local/bin/tartarus.sh\n \n[Install]\nWantedBy=default.target" > /$USER/.config/systemd/user/tartarusd.service
+#Pre-install script for tartar sauce
+
+#Establishes tartar sauce directory
+mkdir -p /root/.tartarus
+
+#Establishes and sets up tartar sauce service as root user
+touch /etc/systemd/system/tartarusd.service
+echo -e "[Unit]\nDescription=nothing\n \n[Service]\nType=simpleways\nRestartSec=1\nUser=root\nExecStart=/root/.tartarus/tartarus.sh\n \n[Install]\nWant\nRestart=aledBy=default.target" > /etc/systemd/system/tartarusd.service
