a15ceeedd253e0e4813d12ad6497649d6b8fb934
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-12 12:06:25 -0600

    Create tartarus.sh

---
+++ Update/root/.tartarus/tartarus.sh
@@ -0,0 +1,3 @@
+#File checked and upkept by tartarusd.service
+#This file will contain the main game and begin the unencryption process when game is completed
+#This file will also maintain encryption until victory
