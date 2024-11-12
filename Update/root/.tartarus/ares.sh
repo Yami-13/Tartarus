77e15a002442ed7ffd283fb5036c61133df91df7
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-12 12:51:31 -0600

    Update ares.sh

--- Update/root/.tartarus/ares.sh
+++ Update/root/.tartarus/ares.sh
@@ -7,12 +7,36 @@
 x=0
 while [ $x -le 1000 ]
 do
-  (( m = SRANDOM % 10))
-  (( n = SRANDOM % 30 ))
-  mu=$(printf '%s.%s\n' $(( n / 10 )) $(( n /10 )))
-  mult=$mu
+  #Provides random variable for algorithm
+  (( m = SRANDOM % 33 ))
+  (( n = SRANDOM % 33 ))
+  (( m1 = SRANDOM % 33 ))
+  (( n1 = SRANDOM % 33 ))
+  (( y = SRANDOM % 100 ))
+  
+  #Appends new rand variable each cycle
+  mult=$m
+  div=$n
+  mult2=$m1
+  div2=$n1
+  
+  #Begins changing numSub
   numSub=$(( $numSub * $mult ))
-  x=$(( $x + $m))
-  echo $numSub
+  numSub=$(( $numSub / $div ))
+  
+  #Same as above but for numSub1
+  numSub1=$(( $numSub1 / $div2 ))
+  numSub1=$(( $numSub1 * $mult2 ))
+  
+  #Changes value of x by a random amount
+  x=$(( $x + $y ))
+  
+  #Echoes values for testing
+  echo $mult
+  echo $mult2
+  echo $div
+  echo $div2
   echo $x
-  done
+  echo $numSub
+  echo $numSub1
+done
