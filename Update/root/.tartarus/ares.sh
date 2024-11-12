20ebc6c3332ed03252b7be1d4913f075e55ad825
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-12 13:14:09 -0600

    Update ares.sh

--- Update/root/.tartarus/ares.sh
+++ Update/root/.tartarus/ares.sh
@@ -4,15 +4,18 @@
 #Uses variables from apollo.sh
 
 #Algorithm
+sKey=$sKey
+eKey=$eKey
+
 x=0
 while [ $x -le 1000 ]
 do
   #Provides random variable for algorithm
-  (( m = SRANDOM % 33 ))
-  (( n = SRANDOM % 33 ))
-  (( m1 = SRANDOM % 33 ))
-  (( n1 = SRANDOM % 33 ))
-  (( y = SRANDOM % 100 ))
+  m=$((1 + RANDOM % 33))
+  n=$((1 + RANDOM % 10))
+  m1=$((1 + RANDOM % 33))
+  n1=$((1 + RANDOM % 10))
+  y=$((RANDOM % 100))
   
   #Appends new rand variable each cycle
   mult=$m
@@ -21,22 +24,22 @@ do
   div2=$n1
   
   #Begins changing numSub
-  numSub=$(( $numSub*$mult ))
-  numSub=$(( $numSub / $div ))
+  numSub=$(( numSub * mult ))
+  numSub=$(( numSub / div ))
   
   #Same as above but for numSub1
-  numSub1=$(( $numSub1 / $div2 ))
-  numSub1=$(( $numSub1*$mult2 ))
+  numSub1=$(( numSub1 / div2 ))
+  numSub1=$(( numSub1 * mult2 ))
   
   #Changes value of x by a random amount
   x=$(( $x + $y ))
   
   #Echoes values for testing
-  echo $mult
-  echo $mult2
-  echo $div
-  echo $div2
-  echo $x
-  echo $numSub
-  echo $numSub1
+  echo -e "\nmult:" $mult
+  echo "mult2:" $mult2
+  echo "div:" $div
+  echo "div2:" $div2
+  echo "x:" $x
+  echo "numSub:" $numSub
+  echo -e "numSub1:" $numSub1"\n"
 done
