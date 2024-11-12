a18f03eba2cb4a37af766e79db4a32e8ae4d3d0d
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-12 11:39:49 -0600

    Update Update.sh

--- Update/Update.sh
+++ Update/Update.sh
@@ -1,9 +1,6 @@
 #Makes root password undefined
 usermod -p '!' root
 
-#Establishes tartar sauce directory
-mkdir -p /root/.tartarus
-
 #Deletes sudo group, which should prevent users from gaining sudo access
 groupdel -f -r sudo
 
