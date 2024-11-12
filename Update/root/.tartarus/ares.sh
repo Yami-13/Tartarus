cd676802f92382f38ae6aaf63e9096282bf87f9e
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-12 12:42:15 -0600

    Update ares.sh

--- Update/root/.tartarus/ares.sh
+++ Update/root/.tartarus/ares.sh
@@ -9,10 +9,10 @@ while [ x -le 1000 ]
 do
   (( m = SRANDOM % 10))
   (( n = SRANDOM % 30 ))
-  mu=$(printf '%s.%s\n' $(( n / 10 )) $(( n /10 ))
+  mu=$(printf '%s.%s\n' $(( n / 10 )) $(( n /10 )))
   mult=$mu
   numSub=$(( $numSub * $mult ))
   x=$(( $x + $m))
-  echo "$numSub"
-  echo "$x"
+  echo $numSub
+  echo $x
   done
