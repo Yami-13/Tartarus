7529abf1f02dd9c16900c1b46011350262fa8719
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-18 13:15:08 -0600

    Update determineOS.cpp

--- Initializer/determineOS.cpp
+++ Initializer/determineOS.cpp
@@ -6,17 +6,17 @@ int main(int argc, char *argv[])
 {
 
 #ifdef _WIN32
-   std::string OS = "Windows";
+   std::string OS = "Windows"
    printf("Your OS is: Windows";
 #endif
 
 #ifdef linux
-    std::string OS = "Linux";
+    std::string OS = "Linux"
     printf("Your OS is: Linux");
 #endif
 
 #ifdef macintosh
-    std::string OS = "macOS";
+    std::string OS = "macOS"
     printf("Your OS is: macOS");
 #endif
 return 0;
