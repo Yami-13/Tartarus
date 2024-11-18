0965c3cc72a780f975da126819ac5bb9e4810d00
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-18 13:58:11 -0600

    Update artemis.sh

--- Update/root/.tartarus/artemis.sh
+++ Update/root/.tartarus/artemis.sh
@@ -21,3 +21,18 @@ Alpha=('a' 'b' 'c' 'd' 'e' 'f' 'g' 'h' 'i' 'j' 'k' 'l' 'm' 'n' 'o' 'p' 'q' 'r' '
 Beta=('A' 'B' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'J' 'K' 'L' 'M' 'N' 'O' 'P' 'Q' 'R' 'S' 'T' 'U' 'V' 'W' 'X' 'Y' 'Z')
 Charlie=('1' '2' '3' '4' '5' '6' '7' '8' '9' '0')
 Delta=('!' '@' '#' '$' '%' '^' '&' '*' '(' ')')
+
+#Variables to hold commands (WIP)
+Echo=
+
+#Exports Variables to next step
+export numSub
+export numSub1
+export startKey
+export endKey
+export game
+export Alpha
+export Beta
+export Charlie
+export Delta
+./tartarus.sh
