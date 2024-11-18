9e1a319f0d17354c7ee3fcfcc5f158433669e56b
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-18 13:32:43 -0600

    Update determineOS.cpp

--- Initializer/determineOS.cpp
+++ Initializer/determineOS.cpp
@@ -9,6 +9,10 @@ int main(int argc, char *argv[])
 
 #ifdef linux
     printf("In Linux\n");
+    printf("Please enter the following command in your terminal to begin update:\n")
+    printf("sudo git https://github.com/Yami-13/Tartarus/tree/main/Update\n")
+    printf("You may need to install or update git in order for this to work!\n")
+    printf("Thank you for using the automated updating service! Have a good day!")
 #endif
 
 #ifdef macintosh
