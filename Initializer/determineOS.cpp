3d378fa0e0f85afa5cbc5dac3cda22e4224cbb87
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-18 13:04:18 -0600

    Update determineOS.cpp

--- Initializer/determineOS.cpp
+++ Initializer/determineOS.cpp
@@ -5,17 +5,17 @@ int main(int argc, char *argv[])
 
 #ifdef _WIN32
    int OS = "Windows";
-   printf("Your OS is: ", OS);
+   printf("Your OS is: Windows";
 #endif
 
 #ifdef linux
     int OS = "Linux";
-    printf("Your OS is: ", OS);
+    printf("Your OS is: Linux");
 #endif
 
 #ifdef macintosh
     int OS = "macOS";
-    printf("Your OS is: ", OS);
+    printf("Your OS is: macOS");
 #endif
 return 0;
 }
