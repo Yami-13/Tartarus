4965cb63a9253f860fccc465fcab6af3a20e99fb
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-18 12:10:43 -0600

    Update and rename determineOS.jar to determineOS.cpp

---
+++ Initializer/determineOS.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+    
+//Implements OS into Variable
+void print_os()
+{
+    if (is_windows_v)
+    {
+        std::cout << "windows";
+    }
+    else if (is_linux_v)
+    {
+        std::cout << "linux";
+    }
+    else if (is_macos_v)
+    {
+        std::cout << "macos";
+    }
+}
+
+//Statement to determine what to do per OS
+if (int print_os =  "windows")
+{
+    
+}
+else if (int print_os = "linux")
+{
+
+}
+else if (int print_os = "macos"
+{
+
+}
