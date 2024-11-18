2862beb35c5e81a996cd0504002755a193ffb0f9
Yami <72431114+Yami-13@users.noreply.github.com>
2024-11-18 13:52:29 -0600

    Update tartarus.sh

--- Update/root/.tartarus/tartarus.sh
+++ Update/root/.tartarus/tartarus.sh
@@ -1,3 +1,11 @@
 #File checked and upkept by tartarusd.service.
 #This file will contain the main game and begin the unencryption process when game is completed.
 #This file will also maintain encryption until victory, and contains decryption key and methods.
+
+#while loop that executes encryption
+while [ $game -le 1 ]
+do
+
+
+done
+ 
